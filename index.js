46c4a9f0451d5ef57d30aa2f20306880a67fdb6a
Gapabhd14 <98918221+Gapabhd14@users.noreply.github.com>
2023-02-27 13:41:16 +0200

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-// onLoad func
-// ===========
-// event click for lessons
-// Michal Blechman, 8/3/22 9/3/22
-window.addEventListener("load", ()=>{
-    setTimeout(()=>{
-        document.getElementById(`img`).style.border = "0.4vw solid red";
-    }, 2000);
-});
